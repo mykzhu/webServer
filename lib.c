1902e8a5f55a55ce2ee2ad8adcdc4917fab4550c
kolka <kolka@kolka-Studio-1535.(none)>
2014-02-23 14:11:50 +0200

    SimpleWeb all major functions

--- lib.c
+++ lib.c
@@ -13,11 +13,269 @@ HYSTORY.....: DATE            COMMENT
 *****************************************************************************/
 /*------------------------IMPORT DECLARATION--------------------------------*/
 #include <stdio.h> 
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>        /*  For select()  */
+#include <unistd.h>          /*  for ssize_t data type  */
+#include <errno.h> 
+#include <fcntl.h>
+#include <time.h>
+
+/*---------------------PUBLIC DECLARATION----------------------------------*/
+#define SERVER "Server: SimpleWeb\n"
+#define CONTENT "Content-Type: text/html\r\n"
+#define CONTENT2 "Content-Type: image/jpeg\r\n"
+
+
+
+struct {
+        char *ext;
+        char *filetype;
+} extensions [] = {
+        {"gif", "image/gif" },  
+        {"jpg", "image/jpeg"}, 
+        {"jpeg","image/jpeg"},
+        {"png", "image/png" },  
+        {"zip", "image/zip" },  
+        {"gz",  "image/gz"  },  
+        {"tar", "image/tar" },  
+        {"htm", "text/html" },  
+        {"html","text/html" },  
+        {"css", "text/css"  },
+        {0,0} };
 
 /*------------------------PUBLIC FUNCTION----------------------------------*/
+/****************************************************************************
+FUNCTION.......: get_time
+DESCRIPTION....: Function which returns current time with us or without.
+ARGUMENTS......: - msg  -string which will contain time value
+                 - flag - if eq. 1-time with us 
+RETURNS........: None
+****************************************************************************/
+void get_time(char *msg, short flag)
+{
+  time_t now;
+  struct tm *tm;
+  struct timeval  tv;
+
+  now = time(0);
+  if ((tm = localtime (&now)) == NULL) 
+  {
+    sprintf(msg,"Error extracting time stuff");
+  }
+  else
+  {
+    if(flag == 1)
+    {
+      gettimeofday(&tv, NULL);
+      sprintf (msg,"[%04d-%02d-%02d %02d:%02d:%02d.%06u]",
+        tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday,
+        tm->tm_hour, tm->tm_min, tm->tm_sec,(int)tv.tv_usec);
+    }
+    else
+    {
+      sprintf (msg,"%04d-%02d-%02d %02d:%02d:%02d",
+        tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday,
+        tm->tm_hour, tm->tm_min, tm->tm_sec);
+    }
+  }
+}
+
+/****************************************************************************
+FUNCTION.......: log_print
+DESCRIPTION....: Function which writes log messages to file.
+ARGUMENTS......: - msg  -string which will contain log message. 
+RETURNS........: None
+****************************************************************************/
 void log_print(char* msg) 
 {
-	printf("%s\n",msg);
+	int fd;
+  char time_msg[40];
+
+	if((fd = open("nweb.log", O_CREAT| O_WRONLY | O_APPEND,0644)) >= 0)
+	{
+    get_time(time_msg, 1);
+		(void)write(fd,time_msg,strlen(time_msg));
+		(void)write(fd,msg,strlen(msg)); 
+		(void)write(fd,"\n",1);      
+		(void)close(fd);
+	}
+}
+
+/****************************************************************************
+FUNCTION.......: notGet
+DESCRIPTION....: Function which sends HTTP response 501 if type of request 
+                 is not GET
+ARGUMENTS......: - sock - socket ID
+RETURNS........: None
+****************************************************************************/
+void notGet(int sock)
+{  
+	char buffer[1024];
+	   
+	//Send HTTP Response line by line
+  strcpy(buffer, "HTTP/1.0 501 Method Not Implemented\r\n");
+  write(sock, buffer, strlen(buffer));
+	strcpy(buffer, SERVER);
+  write(sock, buffer, strlen(buffer));
+	strcpy(buffer, CONTENT);
+	write(sock, buffer, strlen(buffer));
+	strcpy(buffer, "\r\n");
+	write(sock, buffer, strlen(buffer));
+	strcpy(buffer, "<html>\n<head>\n<title>Method Not Implemented</title>\n</head>\r\n");
+	write(sock, buffer, strlen(buffer));
+	strcpy(buffer, "<body>\n<p>501 HTTP request method not supported.</p>\n</body>\n</html>\r\n");
+	write(sock, buffer, strlen(buffer));
+}
+
+/****************************************************************************
+FUNCTION.......: notFound
+DESCRIPTION....: Function which sends HTTP response 404 if requested page is not found
+ARGUMENTS......: - sock - socket ID
+RETURNS........: None
+****************************************************************************/
+void notFound(int sock)
+ {  
+  char buffer[1024]; 
+  long ret;
+  int file_fd;
+
+  //Send HTTP Response line by line
+  strcpy(buffer, "HTTP/1.0 404 Not Found\r\n");
+  write(sock, buffer, strlen(buffer));
+  strcpy(buffer, SERVER);
+  write(sock, buffer, strlen(buffer));
+  if(( file_fd = open("source/404.jpg",O_RDONLY)) == -1) 
+  {
+    log_print("failed to open 404 file");
+    strcpy(buffer, CONTENT);
+    write(sock, buffer, strlen(buffer));
+    strcpy(buffer, "\r\n");
+    write(sock, buffer, strlen(buffer));
+    strcpy(buffer, "<html>\n<head>\n<title>Not Found</title>\n</head>\r\n");
+    write(sock, buffer, strlen(buffer));
+    strcpy(buffer, "<body>\n<p>404 Request file not found.</p>\n</body>\n</html>\r\n");
+    write(sock, buffer, strlen(buffer));
+  }
+  else
+  {
+    strcpy(buffer, CONTENT2);
+    write(sock, buffer, strlen(buffer));
+    strcpy(buffer, "\r\n");
+    write(sock, buffer, strlen(buffer));
+    while ( (ret = read(file_fd, buffer, 1024)) > 0 ) 
+    {
+      (void)write(sock,buffer,ret);
+    }
+  }
+}
+
+/****************************************************************************
+FUNCTION.......: connection
+DESCRIPTION....: Function which receives HTTP requests, parces them and sends
+                 requested file or responce with error messages.
+ARGUMENTS......: - new_socket - socket ID
+RETURNS........: None
+****************************************************************************/
+int connection(int new_socket)
+{
+	   int bufsize = 1024,
+		i,j,buflen, len, file_fd;    
+   	char *buffer = malloc(bufsize);
+   	char * fstr; 
+   	long ret;
+   	char path[] = "site/"; 
+
+   	if(recv(new_socket, buffer, bufsize, 0) == -1)
+   	{
+   		fprintf(stderr, "Error read socket: %s\n", strerror(errno));
+   		return -1;
+   	}
+   	else
+   	{
+      log_print(buffer);
+
+    	if(strncmp(buffer,"GET ",4)&& strncmp(buffer,"get ",4))
+    	{
+			  log_print("Only simple GET operation supported");
+			  notGet(new_socket);
+			  return 0;
+		  }
+		  else
+		  {
+			  for(i=4;i<bufsize;i++) 
+			  { /* null terminate after the second space to ignore extra stuff */
+				  if(buffer[i] == ' ') 
+				  {
+            buffer[i] = 0;
+            break;
+          }
+	      }
+
+	      for(j=0;j<i-1;j++)
+				  if(buffer[j] == '.' && buffer[j+1] == '.')
+				  {
+            log_print("Parent directory (..) path names not supported");
+            return 0;
+          }
+
+          if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) )
+            (void)strcpy(buffer,"GET /site/index.html");
+          else
+          {
+            for (i=strlen(buffer);i>=0;i--)
+            {
+              if (i<5)
+            	{
+            	  buffer[i+strlen(path)] = path[i];
+            	}
+            	else
+            	{
+            		buffer[i+strlen(path)] = buffer[i];
+            	}
+            }
+          }
+
+        	buflen=strlen(buffer);
+        	fstr = (char *)0;
+
+        	for(i=0;extensions[i].ext != 0;i++) 
+        	{
+            len = strlen(extensions[i].ext);
+            if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) 
+            {
+              fstr =extensions[i].filetype;
+              break;
+            }
+        	}
+
+        	if(fstr == 0) 
+        	{
+        		log_print("file extension type not supported");
+        		return 0;
+        	}
+
+        	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) 
+        	{
+        	 	log_print("failed to open file");
+        	 	notFound(new_socket);
+        	}
+          else
+          {
+            (void)sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-Type: %s\r\n\r\n",fstr);
+            (void)write(new_socket,buffer,strlen(buffer));
+            while ( (ret = read(file_fd, buffer, bufsize)) > 0 ) 
+            {
+              (void)write(new_socket,buffer,ret);
+            }
+          }
+          #ifdef LINUX
+            sleep(1);       /* to allow socket to drain */
+          #endif
+		    }
+      }  
+      return 0;
 }
 //---------------------------------------------------------------------------
 //End of file lib.c:
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 FILE........: main.c
 AUTHOR......: Zhuravel Mykola
 DESCRIPTION.: File contents main function of simple web server main.c
-FUNCTION....: - 
+FUNCTION....: - int main(int argc, void **argv)
 NOTES.......: 
 COPYRIGHT...: All rights reserved by Zuravel Mykola
 HYSTORY.....: DATE            COMMENT
@@ -16,21 +16,29 @@ HYSTORY.....: DATE            COMMENT
 #include <stdlib.h> 
 #include <dlfcn.h> 
 #include <netdb.h>
-#include <sys/socket.h>
+#include <sys/socket.h> /*  socket definitions        */
 #include <errno.h> 
 #include <string.h> 
 /*---------------------PUBLIC DECLARATION----------------------------------*/
-#define PORT "3456" // the port users will be connecting to
+#define PORT "8080" // the port users will be connecting to
 
 #define BACKLOG 10  // how many pending connections queue will hold
 
 //---------------------------------------------------------------------------
 
 /*------------------------PUBLIC FUNCTION----------------------------------*/
+/****************************************************************************
+FUNCTION.......: main
+DESCRIPTION....: Function which open library of functions and 
+                 create process which will handle connection.
+ARGUMENTS......:
+RETURNS........: 
+****************************************************************************/
 int main(int argc, void **argv)
 {
   char *error; 
   char clientIpAddress[16]; 
+  char msg[255];
   memset(clientIpAddress, '\0', 16);
   void *libHandle;  
 
@@ -39,26 +47,35 @@ int main(int argc, void **argv)
       new_id, 
       yes=1, 
       flag = 1,
-      clientStructSize =0;
+      clientStructSize =0,
+      pid;
 
   struct addrinfo *hints, *servinfo, *struct_point;
   struct sockaddr_in *client;
   socklen_t sin_size;
 
   void (*log_print) (char*); 
+  int (*connection) (int);
  
   libHandle = dlopen("./lib.so", RTLD_NOW); 
 
   if (NULL == libHandle) 
   { 
-    fprintf(stderr, "%s\n", dlerror()); 
+    fprintf(stderr, "Error during opening lib %s\n", dlerror()); 
     exit(EXIT_FAILURE); 
   } 
 
   *(void **) (&log_print) = dlsym(libHandle, "log_print"); 
   if ((error = dlerror()) != NULL) 
   {
-    fprintf(stderr, "%s\n", error); 
+    fprintf(stderr, "Error during call log_print %s\n", error); 
+    exit(EXIT_FAILURE); 
+  } 
+
+  *(int **) (&connection) = dlsym(libHandle, "connection"); 
+  if ((error = dlerror()) != NULL) 
+  {
+    fprintf(stderr, "Error during call connection %s\n", error); 
     exit(EXIT_FAILURE); 
   } 
   
@@ -72,7 +89,8 @@ int main(int argc, void **argv)
 
   if((iret_value = getaddrinfo(NULL, PORT, hints, &servinfo)) != 0)
   {
-    fprintf(stderr, "Error getting addres: %s\n", gai_strerror(iret_value));
+    sprintf(msg, "Error getting addres: %s\n", gai_strerror(iret_value));
+    (*log_print)(msg);
     free(hints);
     dlclose(libHandle); 
     exit(EXIT_FAILURE);
@@ -84,12 +102,14 @@ int main(int argc, void **argv)
     if ((isockId = socket(struct_point->ai_family, struct_point->ai_socktype,
          struct_point->ai_protocol)) == -1)
     {
-      fprintf(stderr, "Error creadting socket: %s\n", strerror(errno));
+      sprintf(msg, "Error creadting socket: %s\n", strerror(errno));
+      (*log_print)(msg);
       continue;
     }
     if (setsockopt(isockId, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1) 
     {
-      fprintf(stderr, "Error socket options: %s\n", strerror(errno));
+      sprintf(msg, "Error socket options: %s\n", strerror(errno));
+      (*log_print)(msg);
       free(hints);
       dlclose(libHandle);
       exit(EXIT_FAILURE);
@@ -97,7 +117,8 @@ int main(int argc, void **argv)
     if (bind(isockId, struct_point->ai_addr, struct_point->ai_addrlen) == -1) 
     {
       close(isockId);
-      fprintf(stderr, "Error connecting socket to addres: %s\n", strerror(errno));
+      sprintf(msg, "Error connecting socket to addres: %s\n", strerror(errno));
+      (*log_print)(msg);
       continue;
     }
     break;
@@ -105,7 +126,8 @@ int main(int argc, void **argv)
 
   if (struct_point == NULL)  
   {
-    fprintf(stderr, "server: failed to bind\n");
+    sprintf(msg, "server: failed to bind\n");
+    (*log_print)(msg);
     free(hints);
     dlclose(libHandle); 
     return 2;
@@ -115,7 +137,8 @@ int main(int argc, void **argv)
 
   if (listen(isockId, BACKLOG) == -1) 
   {
-    fprintf(stderr, "server: failed to listen\n");
+    sprintf(msg, "server: failed to listen\n");
+    (*log_print)(msg);
     free(hints); 
     close(isockId);
     dlclose(libHandle); 
@@ -125,6 +148,7 @@ int main(int argc, void **argv)
   //TODO: add reap all dead processes
 
   (*log_print)("Server waiting for connections...");
+
   while(flag)
   {
     client = (struct sockaddr_in*) calloc(1, sizeof(struct sockaddr_in)); 
@@ -133,19 +157,53 @@ int main(int argc, void **argv)
     new_id = accept(isockId, (struct sockaddr *)client, &clientStructSize);
     if (new_id == -1) 
     {
-      fprintf(stderr, "server: failed to accept\n");
+      sprintf(msg, "server: failed to accept\n");
+      (*log_print)(msg);
       continue;
     }
-    inet_ntop(AF_INET, &(client->sin_addr), clientIpAddress, sizeof(clientIpAddress)); 
-    fprintf( stdout, "Connection from client [%s:%d]\n", clientIpAddress, 
-    ntohs(client->sin_port) ); 
+    else
+    {
+      inet_ntop(AF_INET, &(client->sin_addr), clientIpAddress, sizeof(clientIpAddress)); 
+      sprintf( msg, "Connection from client [%s:%d]\n", clientIpAddress, 
+      ntohs(client->sin_port) ); 
+      (*log_print)(msg);
+
+      pid = fork();
+      if(0 == pid)
+      { // this is child's context
+        close(isockId);//child doesn't need the listener
+        if((*connection)(new_id) == -1)
+        {
+          free(hints);
+          close(new_id);
+          (*log_print)("Error in connection function!");
+          dlclose(libHandle);
+          exit(EXIT_FAILURE); 
+        };
+        free(hints);
+        close(new_id);
+        exit(EXIT_SUCCESS);
+      }
+      else if (0 < pid)
+      { // this is parent's context
+        sprintf(msg, "New child %d was created.\n",pid);
+        (*log_print)(msg);
+        close(new_id);
+      }
+      else
+      {
+        sprintf(msg, "server: failed to fork\n");
+        (*log_print)(msg);
+        continue; 
+      }
+    }
     //flag = 0;
   }
 
   free(hints);
   (*log_print)("Server was stoped");
   dlclose(libHandle); 
-  exit(EXIT_SUCCESS); 
+  exit(EXIT_FAILURE); 
 }
 //---------------------------------------------------------------------------
 //End of file main.c:
